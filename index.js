84aee3edd5086c8c42ce91fae9951fb95e7c8af5
cguevara6 <cguevara6@dxc.com>
2023-12-15 18:40:52 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+const express = require("express")
+const path = require("path")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const fs = require("fs")
+
+const employeesRouter = require("./routes/employees.routes")
+
+const port = 3000
+
+var wStream = fs.createWriteStream(path.join(__dirname,"log", "serverLog.txt"), {flags:"a"})
+
+var app = express()
+
+app.use(morgan("combined", {stream:wStream}))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.use((request, response, next) => {
+    request.rootDirName = __dirname
+    next()
+})
+
+app.use("/employees", employeesRouter)
+
+app.listen(port, () => {
+    console.log(`Server started at port : ${port}`)
+})
+
+module.exports = app
---
+++ model/employees.js
@@ -0,0 +1,16 @@
+class Employees {
+    empId
+    empName
+    timeIn
+    temperature
+
+    constructor(empId, empName, timeIn, temperature) {
+        this.empId = empId
+        this.empName = empName
+        this.timeIn = timeIn
+        this.temperature = temperature
+    }
+
+}
+
+module.exports = Employees

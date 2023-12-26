87de28ce482a77fe7ad27d6bdc2a8b7be78ac586
cguevara6 <cguevara6@dxc.com>
2023-12-26 20:48:16 +0800

    fix: add null checker for param.searchBy

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -5,8 +5,7 @@ exports.getRecord = async (request, response, next) => {
         const connection = await connectionRequest()
         const param = request.query
         
-        var sqlQuery = `
-            SELECT CONCAT(
+        var sqlQuery = `SELECT CONCAT(
                     p.first_name,
                     IF(LENGTH(p.middle_name) > 0, CONCAT(' ', p.middle_name), ''),
                     ' ',
@@ -17,29 +16,25 @@ exports.getRecord = async (request, response, next) => {
             JOIN person p ON m.person_id = p.person_id
             JOIN location l ON m.location_id = l.location_id `
 
-        if (param.searchBy.toLowerCase() === "name" && param.searchKey) {
-            sqlQuery += `
-            WHERE CONCAT(p.first_name, ' ', p.middle_name, ' ', p.last_name) LIKE '%${param.searchKey}%' `
+        if (param.searchBy && param.searchKey) {
+            if (param.searchBy.toLowerCase() === "name") {
+                sqlQuery += `WHERE CONCAT(p.first_name, ' ', p.middle_name, ' ', p.last_name) LIKE '%${param.searchKey}%' `
+            }
+
+            if (param.searchBy.toLowerCase() === "date_of_access") {
+                sqlQuery += `WHERE m.date_of_access = '${param.searchKey}' `
+            }
+
+            if (param.searchBy.toLowerCase() === "location") {
+                sqlQuery += `WHERE l.location_name LIKE '%${param.searchKey}%' `
+            }
         }
 
-        if (param.searchBy.toLowerCase() === "date_of_access" && param.searchKey) {
-            sqlQuery += `
-            WHERE m.date_of_access = '${param.searchKey}' `
-        }
-
-        if (param.searchBy.toLowerCase() === "location" && param.searchKey) {
-            sqlQuery += `
-            WHERE l.location_name LIKE '%${param.searchKey}%' `
-        }
-
-        sqlQuery += `
-            ORDER BY ${param.orderBy || 'date_of_access'} ${param.order || 'DESC'} `
-        sqlQuery += `
-            LIMIT 20 `
+        sqlQuery += `ORDER BY ${param.orderBy || 'date_of_access'} ${param.order || 'DESC'} `
+        sqlQuery += `LIMIT 20 `
 
         if (param.page) {
-            sqlQuery += `
-            OFFSET ${(param.page - 1) * 20}`
+            sqlQuery += `OFFSET ${(param.page - 1) * 20}`
         }
 
         console.log(sqlQuery)

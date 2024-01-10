fd61b0c899deef6b297b4fd8252299a446f597e5
cguevara6 <cguevara6@dxc.com>
2024-01-10 17:55:59 +0800

    fix: add port in database connection

--- utils.js
+++ utils.js
@@ -47,6 +47,7 @@ exports.validateToken = (request, response, next)  => {
 exports.connectionRequest = () => {
     return mySQLClient.createConnection({
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
